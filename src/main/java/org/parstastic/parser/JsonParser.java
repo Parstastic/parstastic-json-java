c17f4d4da400b0d4d56b5d04d790b52c405d817b
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-05 22:10:33 +0200

    feat: add JsonParser

---
+++ src/main/java/org/parstastic/parser/JsonParser.java
@@ -0,0 +1,13 @@
+package org.parstastic.parser;
+
+/**
+ * This class encapsulates behavior for general <code>JSON</code> parsing.
+ */
+public class JsonParser {
+    /**
+     * Creates a {@link JsonParser} object.
+     */
+    public JsonParser() {
+        super();
+    }
+}
