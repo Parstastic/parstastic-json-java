3b50b7ef62e9b9d8bc6f4e3b6efc1428cb144774
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-05 21:50:19 +0200

    feat: add InvalidJsonException

---
+++ src/main/java/org/parstastic/parser/exceptions/InvalidJsonException.java
@@ -0,0 +1,20 @@
+package org.parstastic.parser.exceptions;
+
+/**
+ * Exceptions of this type are thrown whenever there is a problem when parsing a <code>JSON</code> {@link String}.
+ */
+public class InvalidJsonException extends RuntimeException {
+    /**
+     * Creates an {@link InvalidJsonException} without a message
+     */
+    public InvalidJsonException() {
+        super();
+    }
+
+    /**
+     * Creates an {@link InvalidJsonException} with a message
+     */
+    public InvalidJsonException(final String message) {
+        super(message);
+    }
+}
