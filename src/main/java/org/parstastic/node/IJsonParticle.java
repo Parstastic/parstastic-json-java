709f1e48f3ad3ec5daa1145a56b102dc9b647529
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-06 01:57:58 +0200

    feat: add IJsonParticle

---
+++ src/main/java/org/parstastic/node/IJsonParticle.java
@@ -0,0 +1,8 @@
+package org.parstastic.node;
+
+/**
+ * This interface represents anything that can be part of a <code>JSON</code> string.
+ * Included are both nodes and key-value pairs of object nodes.
+ */
+public interface IJsonParticle {
+}
