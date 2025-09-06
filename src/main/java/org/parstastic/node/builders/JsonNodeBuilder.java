6eafb473ffa41a65b77a714bcc5edef384bca670
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-06 18:25:59 +0200

    feat: add JsonNodeBuilder

---
+++ src/main/java/org/parstastic/node/builders/JsonNodeBuilder.java
@@ -0,0 +1,38 @@
+package org.parstastic.node.builders;
+
+import org.parstastic.node.JsonNode;
+import org.parstastic.parser.IJsonParser;
+import org.parstastic.parser.JsonParser;
+import org.parstastic.parser.exceptions.InvalidJsonException;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * This abstract class provides generalized methods to parse <code>JSON</code> strings into {@link JsonNode} objects.
+ *
+ * @param <T> type of the parsed {@link JsonNode} object
+ * @param <E> type of {@link InvalidJsonException} that can be thrown during parsing
+ */
+public abstract class JsonNodeBuilder<T extends JsonNode, E extends InvalidJsonException> implements IJsonParser<T, E> {
+    protected final JsonParser jsonParser;
+
+    /**
+     * Creates a {@link JsonNodeBuilder} object with given {@link JsonParser}.
+     *
+     * @param jsonParser {@link JsonParser} that can be invoked for parsing
+     */
+    protected JsonNodeBuilder(final JsonParser jsonParser) {
+        super();
+        this.jsonParser = jsonParser;
+    }
+
+    /**
+     * Determines whether a given <code>JSON</code> {@link String} can be parsed by this {@link JsonNodeBuilder} object.
+     *
+     * @param json <code>JSON</code> {@link String} to parse object from
+     * @param index index in the <code>JSON</code> {@link String} to start parsing from
+     * @return {@code true} if this {@link JsonNodeBuilder} can parse the <code>JSON</code> {@link String} into an object,
+     *         {@code false} otherwise
+     */
+    public abstract boolean canParseJson(final String json, final AtomicInteger index);
+}
