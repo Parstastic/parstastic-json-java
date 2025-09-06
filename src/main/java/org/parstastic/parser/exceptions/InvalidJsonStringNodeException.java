b7c2e063863d8023cb57d1ac06414e5b0d4fe055
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-07 00:41:06 +0200

    feat: add InvalidJsonStringNodeException

---
+++ src/main/java/org/parstastic/parser/exceptions/InvalidJsonStringNodeException.java
@@ -0,0 +1,22 @@
+package org.parstastic.parser.exceptions;
+
+import org.parstastic.node.string.StringNode;
+
+/**
+ * Exception thrown whenever there is a problem parsing a {@link StringNode}.
+ */
+public class InvalidJsonStringNodeException extends InvalidJsonException {
+    /**
+     * Creates an {@link InvalidJsonStringNodeException} without a message
+     */
+    public InvalidJsonStringNodeException() {
+        super();
+    }
+
+    /**
+     * Creates an {@link InvalidJsonStringNodeException} with a message
+     */
+    public InvalidJsonStringNodeException(final String message) {
+        super(message);
+    }
+}
