a539910b83698f06c23291f83ab4d0d2c96e9919
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-07 03:16:24 +0200

    docs: update javadoc on IJsonParticle

--- src/main/java/org/parstastic/node/IJsonParticle.java
+++ src/main/java/org/parstastic/node/IJsonParticle.java
@@ -1,8 +1,13 @@
 package org.parstastic.node;
 
+import static org.parstastic.node.object.ObjectNode.ObjectNodeProperty;
+
 /**
- * This interface represents anything that can be part of a <code>JSON</code> string.
+ * This interface represents anything that can be part of a <code>JSON</code> {@link String}.
  * Included are both nodes and key-value pairs of object nodes.
+ *
+ * @see JsonNode
+ * @see ObjectNodeProperty
  */
 public interface IJsonParticle {
 }
