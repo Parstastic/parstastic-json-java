cf4aa91c1f1b10953aeca1ba6f4f3ee0856bd033
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-09-19 01:52:28 +0200

    feat: add InvalidJsonObjectNodePropertyException

---
+++ src/main/java/org/parstastic/jparstastic_json/parser/exceptions/InvalidJsonObjectNodePropertyException.java
@@ -0,0 +1,36 @@
+package org.parstastic.jparstastic_json.parser.exceptions;
+
+import static org.parstastic.jparstastic_json.node.object.ObjectNode.ObjectNodeProperty;
+
+/**
+ * Exception thrown whenever there is a problem parsing an {@link ObjectNodeProperty}.
+ */
+public class InvalidJsonObjectNodePropertyException extends InvalidJsonException {
+    /**
+     * Creates an {@link InvalidJsonObjectNodePropertyException} without a message or a cause
+     */
+    public InvalidJsonObjectNodePropertyException() {
+        super();
+    }
+
+    /**
+     * Creates an {@link InvalidJsonObjectNodePropertyException} with a message
+     */
+    public InvalidJsonObjectNodePropertyException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Creates an {@link InvalidJsonObjectNodePropertyException} with a cause
+     */
+    public InvalidJsonObjectNodePropertyException(final InvalidJsonException cause) {
+        super(cause);
+    }
+
+    /**
+     * Creates an {@link InvalidJsonObjectNodePropertyException} with a message and a cause
+     */
+    public InvalidJsonObjectNodePropertyException(final String message, final InvalidJsonException cause) {
+        super(message, cause);
+    }
+}
