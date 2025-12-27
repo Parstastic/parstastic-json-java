77f0359c367ed57bc0cd517104a883ab0d62912b
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-12-28 00:01:13 +0100

    feat: add JsonValue

---
+++ src/main/java/org/parstastic/jparstastic_json/node/JsonValue.java
@@ -0,0 +1,19 @@
+package org.parstastic.jparstastic_json.node;
+
+public class JsonValue extends JsonParticle {
+    private final Whitespace leadingWhitespace;
+    private final JsonNode jsonNode;
+    private final Whitespace trailingWhitespace;
+
+    public JsonValue(final Whitespace leadingWhitespace, final JsonNode jsonNode, final Whitespace trailingWhitespace) {
+        super();
+        this.leadingWhitespace = leadingWhitespace;
+        this.jsonNode = jsonNode;
+        this.trailingWhitespace = trailingWhitespace;
+    }
+
+    @Override
+    public String stringify(final StringifyOptions options) {
+        return this.jsonNode.stringify(options);
+    }
+}
