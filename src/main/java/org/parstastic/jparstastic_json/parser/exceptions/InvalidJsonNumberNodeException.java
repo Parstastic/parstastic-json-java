e4346bcb0ecb565bd108131e8ac9cb7b7a7074ed
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2025-12-29 13:58:36 +0100

    refactor: move NumberNode to nodes package

--- src/main/java/org/parstastic/jparstastic_json/parser/exceptions/InvalidJsonNumberNodeException.java
+++ src/main/java/org/parstastic/jparstastic_json/parser/exceptions/InvalidJsonNumberNodeException.java
@@ -1,6 +1,6 @@
 package org.parstastic.jparstastic_json.parser.exceptions;
 
-import org.parstastic.jparstastic_json.node.number.NumberNode;
+import org.parstastic.jparstastic_json.node.nodes.NumberNode;
 
 /**
  * Exception thrown whenever there is a problem parsing a {@link NumberNode}.
