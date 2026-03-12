06f9c46354a031daa108b495a1b8d74f4295352a
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-03-13 00:45:53 +0100

    feat: fully implement JSON API

--- src/main/java/org/parstastic/jparstastic_json/JSON.java
+++ src/main/java/org/parstastic/jparstastic_json/JSON.java
@@ -1,17 +1,24 @@
 package org.parstastic.jparstastic_json;
 
-import org.parstastic.jparstastic_json.node.nodes.JsonNode;
+import org.parstastic.jparstastic_json.node.JsonParticle;
+import org.parstastic.jparstastic_json.node.JsonValue;
+import org.parstastic.jparstastic_json.parser.FullStringJsonParser;
+import org.parstastic.jparstastic_json.parser.JsonParsingResult;
 
 public final class JSON {
     private JSON() throws IllegalStateException {
         throw new IllegalStateException();
     }
 
-    public static String stringify(final JsonNode jsonNode) {
-        return jsonNode.stringify();
+    public static String stringify(final JsonParticle jsonParticle) {
+        return jsonParticle.stringify();
     }
 
-    public static JsonNode parse(final String json) {
-        throw new UnsupportedOperationException();
+    public static JsonValue parse(final String json) {
+        try {
+            return new FullStringJsonParser().parse(json).getValue();
+        } catch (final JsonParsingResult.JsonParsingResultNoSuchElementException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
