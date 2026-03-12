450e22ae85de4d3685e7342417019a933be9d1d6
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-03-13 00:32:51 +0100

    feat: add NullNodeParser

---
+++ src/main/java/org/parstastic/jparstastic_json/parser/parsers/nodes/NullNodeParser.java
@@ -0,0 +1,35 @@
+package org.parstastic.jparstastic_json.parser.parsers.nodes;
+
+import org.parstastic.jparstastic_json.node.JsonParticleInstantiationException;
+import org.parstastic.jparstastic_json.node.nodes.NullNode;
+import org.parstastic.jparstastic_json.parser.JsonParsingProcess;
+import org.parstastic.jparstastic_json.parser.steps.BlockStep;
+import org.parstastic.jparstastic_json.parser.steps.JsonParsingStep;
+import org.parstastic.jparstastic_json.parser.steps.ParseCharacterStep;
+
+import java.util.List;
+
+public class NullNodeParser extends JsonNodeParser<NullNode> {
+    @Override
+    public boolean canParse(final JsonParsingProcess parsingProcess) {
+        return parsingProcess.startsWith(NullNode.STRING_VALUE);
+    }
+
+    @Override
+    protected JsonParsingStep getStep() {
+        return new BlockStep(
+                createValidation().toArray(new JsonParsingStep[NullNode.STRING_VALUE.length()])
+        );
+    }
+
+    private List<ParseCharacterStep> createValidation() {
+        return NullNode.STRING_VALUE.chars()
+                .mapToObj(i -> new ParseCharacterStep((char) i))
+                .toList();
+    }
+
+    @Override
+    protected NullNode create() throws JsonParticleInstantiationException {
+        return NullNode.NULL_NODE;
+    }
+}
