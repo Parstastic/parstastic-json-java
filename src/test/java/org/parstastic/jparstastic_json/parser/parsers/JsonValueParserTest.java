759f869581a54b0a656f01fad3d0b6895b8a652a
Tom Weelborg <135610355+tom-weelborg@users.noreply.github.com>
2026-03-29 01:01:39 +0100

    test: add JsonValueParserTest

---
+++ src/test/java/org/parstastic/jparstastic_json/parser/parsers/JsonValueParserTest.java
@@ -0,0 +1,87 @@
+package org.parstastic.jparstastic_json.parser.parsers;
+
+import org.parstastic.jparstastic_json.node.JsonParticle;
+import org.parstastic.jparstastic_json.node.JsonParticleInstantiationException;
+import org.parstastic.jparstastic_json.node.JsonValue;
+import org.parstastic.jparstastic_json.node.Whitespace;
+import org.parstastic.jparstastic_json.node.nodes.StringNode;
+import org.parstastic.jparstastic_json.parser.JsonParsingProcess;
+import org.parstastic.jparstastic_json.parser.JsonParsingResult;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class JsonValueParserTest extends JsonParticleParserTest {
+    @Override
+    protected JsonParticleParser<?> getInstance() {
+        return new JsonValueParser();
+    }
+
+    @Override
+    protected Map<String, JsonParticle> getValidTargets() throws JsonParticleInstantiationException {
+        return mergeMaps(getSingleValidTargets(), getDuplicateValidTargets());
+    }
+
+    protected Map<String, JsonParticle> getSingleValidTargets() throws JsonParticleInstantiationException {
+        return Map.of(
+                "\"test\"", new JsonValue(new Whitespace(""), new StringNode("test"), new Whitespace("")),
+                " \"test\" ", new JsonValue(new Whitespace(" "), new StringNode("test"), new Whitespace(" ")),
+                "\"test\" ", new JsonValue(new Whitespace(""), new StringNode("test"), new Whitespace(" ")),
+                " \"test\"", new JsonValue(new Whitespace(" "), new StringNode("test"), new Whitespace(""))
+        );
+    }
+
+    protected Map<String, JsonParticle> getDuplicateValidTargets() throws JsonParticleInstantiationException {
+        return getSingleValidTargets().entrySet().stream()
+                .map(e -> new AbstractMap.SimpleEntry<>(
+                        e.getKey() + e.getKey().trim(),
+                        e.getValue()
+                ))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue
+                ));
+    }
+
+    @Override
+    protected Map<String, JsonParsingResult.JsonParsingResultError> getInvalidTargets() {
+        return Map.of();
+    }
+
+    @Override
+    protected Map<String, JsonParsingResult.JsonParsingResultError> getParseInvalidTargets() {
+        final JsonParsingProcess parsingProcessOneSpace = new JsonParsingProcess(" ");
+        parsingProcessOneSpace.incrementIndex();
+
+        final JsonParsingProcess parsingProcessTwoSpaces = new JsonParsingProcess("  ");
+        parsingProcessTwoSpaces.incrementIndex();
+        parsingProcessTwoSpaces.incrementIndex();
+
+        return mergeMaps(
+                super.getParseInvalidTargets(),
+                Map.of(
+                        "",
+                        new JsonParsingResult.JsonParsingResultError(
+                                "Exporting failed",
+                                new JsonParsingProcess("")
+                        ),
+                        " ",
+                        new JsonParsingResult.JsonParsingResultError(
+                                "Exporting failed",
+                                parsingProcessOneSpace
+                        ),
+                        "  ",
+                        new JsonParsingResult.JsonParsingResultError(
+                                "Exporting failed",
+                                parsingProcessTwoSpaces
+                        ),
+                        "text",
+                        new JsonParsingResult.JsonParsingResultError(
+                                "Exporting failed",
+                                new JsonParsingProcess("text")
+                        )
+                )
+        );
+    }
+}
